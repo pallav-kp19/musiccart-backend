c01b9586d7cb4dbea0a4c178ea662bcb042b2085
Pallav Kumar Patra <73886524+Pallav-19@users.noreply.github.com>
2024-07-22 09:37:23 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://musicart-mu.vercel.app'
+    'https://musicart-mu.vercel.app',
+    'http://musicart.pallav.site',
+    'https://musicart.pallav.site',
+
 ]
 export default allowedOrigins
